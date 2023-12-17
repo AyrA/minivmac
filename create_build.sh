c917410614cb1f402a1b98a4797aac1461fc0470
AyrA <github@bitm.sg>
2023-12-17 22:31:52 +0100

    Update build script

--- create_build.sh
+++ create_build.sh
@@ -1,1 +1,8 @@
-./setup_t -t wx64 -hres 512 -vres 342 -depth 0 -magnify 1 -mf 2 -sound 1 -iid 1 -speed z -bg 1 -im 1 -e mgw
+chmod a+x setup_t
+rm build.sh
+./setup_t -t wx64 -hres 512 -vres 342 -depth 0 -magnify 1 -mf 2 -sound 1 -iid 1 -speed z -bg 1 -im 1 -e mvc -maintainer AyrA -homepage https://github.com/AyrA/minivmac > build.sh
+chmod a+x build.sh
+./build.sh
+make clean
+make
+
